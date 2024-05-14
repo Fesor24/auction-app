725af86a831fb04ea2b3c5fe8193886877ded52c
Wale <onafesowale@gmail.com>
2024-05-14 22:05:12 +0100

    repository

---
+++ src/AuctionService/Repository/AuctionRepository.cs
@@ -0,0 +1,17 @@
+﻿using AuctionService.Data;
+using AuctionService.Domain;
+using AuctionService.Interfaces;
+
+namespace AuctionService.Repository;
+
+public class AuctionRepository : GenericRepository<Auction>, IAuctionRepository
+{
+    private readonly AuctionDbContext _context;
+
+    public AuctionRepository(AuctionDbContext context): base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync() => await _context.CompleteAsync();
+}
---
+++ src/AuctionService/Repository/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using Shared.Interfaces;
+using System.Linq.Expressions;
+
+namespace AuctionService.Repository;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+{
+    private readonly AuctionDbContext _context;
+
+    public GenericRepository(AuctionDbContext context) => _context = context;
+
+    public async Task AddAsync(TEntity entity) =>
+        await _context.Set<TEntity>().AddAsync(entity);
+
+    public void Delete(TEntity entity) =>
+        _context.Set<TEntity>().Remove(entity);
+
+    public async Task<ICollection<TEntity>> GetAllAsync() =>
+        await _context.Set<TEntity>().ToListAsync();
+
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> criteria) =>
+        await _context.Set<TEntity>().FirstOrDefaultAsync(criteria);
+
+    public void Update(TEntity entity)
+    {
+        _context.Attach(entity);
+
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+}
