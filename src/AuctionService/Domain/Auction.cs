277b57d34f50986162827ae16d6c140a3b0e0919
Wale <onafesowale@gmail.com>
2024-05-14 22:03:09 +0100

    entities

---
+++ src/AuctionService/Domain/Auction.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.Enums;
+using Shared.Entity;
+
+namespace AuctionService.Domain;
+
+public class Auction : BaseEntity
+{
+    public decimal ReservePrice { get; set; }
+    public string Seller { get; set; } = string.Empty;
+    public string Winner { get; set; } = string.Empty;
+    public decimal? SoldAmount { get; set; }
+    public decimal? CurrentHighBid { get; set; }
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdateAt { get; set;} = DateTime.UtcNow;
+    public DateTime AuctionEnd { get; set; }
+    public Status Status { get; set; }
+    public Item Item { get; set; }
+}
---
+++ src/AuctionService/Domain/Item.cs
@@ -0,0 +1,22 @@
+﻿using AuctionService.Enums;
+using Shared.Entity;
+
+namespace AuctionService.Domain;
+
+public class Item : BaseEntity
+{
+    public string Title { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+    public Dimension Dimension { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Style Style { get; set; }
+    public Medium Medium { get; set; }
+    public string CurrentLocation { get; set; } = string.Empty;
+    public decimal Value { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public bool IsAuthenticated { get; set; }
+    public Guid AuctionId { get; set; }
+    public Auction Auction { get; set; }
+}
+
+public record Dimension(decimal Height, decimal Width, decimal Depth);
