c953e1acba0be77ebafb772786fe8a408ef2519d
Wale <onafesowale@gmail.com>
2024-05-14 22:03:50 +0100

    create auction command

---
+++ src/AuctionService/Features/Auction/Commands/Create/CreateAuctionCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Shared.Response;
+
+namespace AuctionService.Features.Auction.Commands.Create;
+
+internal record CreateAuctionCommand() : IRequest<Result<bool>>;
+
+internal sealed class CreateAuctionCommandHandler : IRequestHandler<CreateAuctionCommand, Result<bool>>
+{
+    public async Task<Result<bool>> Handle(CreateAuctionCommand request, CancellationToken cancellationToken)
+    {
+        return await Task.FromResult(true);
+    }
+}
