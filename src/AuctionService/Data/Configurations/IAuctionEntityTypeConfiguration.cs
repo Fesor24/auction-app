3f885a8d2ca3f2e21d6f42b81856b5ea4a376d3d
Wale <onafesowale@gmail.com>
2024-05-14 22:03:00 +0100

    entity configurations

---
+++ src/AuctionService/Data/Configurations/IAuctionEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.Domain;
+using AuctionService.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuctionService.Data.Configurations;
+
+internal sealed class IAuctionEntityTypeConfiguration : IEntityTypeConfiguration<Auction>
+{
+    public void Configure(EntityTypeBuilder<Auction> builder)
+    {
+        builder.ToTable(nameof(Auction), "auc");
+
+        builder.Property(p => p.Status)
+            .HasConversion(c => c.ToString(),
+            v => (Status)Enum.Parse(typeof(Status), v));
+    }
+}
---
+++ src/AuctionService/Data/Configurations/IItemEntityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AuctionService.Domain;
+using AuctionService.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuctionService.Data.Configurations;
+
+internal sealed class IItemEntityConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.ToTable(nameof(Item), "auc");
+
+        builder.OwnsOne(item => item.Dimension)
+            .WithOwner();
+
+        builder.Property(p => p.Style)
+            .HasConversion(c => c.ToString(),
+            v => (Style)Enum.Parse(typeof(Style), v));
+
+        builder.Property(p => p.Medium)
+            .HasConversion(c => c.ToString(),
+            v => (Medium)Enum.Parse(typeof(Medium), v));
+    }
+}
