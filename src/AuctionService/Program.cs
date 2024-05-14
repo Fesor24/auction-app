ed430fb11206bc9ede390c70bb9bb87fbdc83b43
Wale <onafesowale@gmail.com>
2024-05-14 22:06:16 +0100

    add services

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,21 @@
+using AuctionService.Extensions;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.RegisterServices(builder.Configuration);
+
+builder.Services.AddSerilog();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseSerilogRequestLogging();
 
 app.Run();
