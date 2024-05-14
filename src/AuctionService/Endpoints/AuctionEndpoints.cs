39a6420cfc3a411828fa90005655ff0c532ea2bb
Wale <onafesowale@gmail.com>
2024-05-14 22:03:19 +0100

    auction endpoints

---
+++ src/AuctionService/Endpoints/AuctionEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using AuctionService.Features.Auction.Commands.Create;
+using MediatR;
+
+namespace AuctionService.Endpoints;
+
+public static class AuctionEndpoints
+{
+    public static void Register(this WebApplication app)
+    {
+        const string ENDPOINT = "api/auction";
+
+        app.MapPost(ENDPOINT, async (ISender sender) =>
+        {
+            var result = await sender.Send(new CreateAuctionCommand());
+
+            return result.Match
+                (val => Results.CreatedAtRoute(), err => Results.BadRequest(err));
+        });
+    }
+}
