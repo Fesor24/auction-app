5e75f8e422b11169bf1a7b690279984886c2aab0
Wale <onafesowale@gmail.com>
2024-05-14 22:02:22 +0100

    db context

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AuctionService.Domain;
+using Microsoft.EntityFrameworkCore;
+using Shared.Interfaces;
+using System.Reflection;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext, IUnitOfWork
+{
+    public AuctionDbContext(DbContextOptions options): base(options)
+    {
+        
+    }
+
+    public DbSet<Auction> Auction => Set<Auction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach(var property in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(t => t.GetProperties()
+            .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?))))
+        {
+            property.SetColumnType("decimal(18,2)");
+        }
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public async Task<int> CompleteAsync() =>
+       await base.SaveChangesAsync();
+}
