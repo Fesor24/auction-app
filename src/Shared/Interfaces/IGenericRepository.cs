7d87fb539957bc53a56a8fb93b472f0e5602fb9c
Wale <onafesowale@gmail.com>
2024-05-14 22:05:28 +0100

    generic repo

---
+++ src/Shared/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Shared.Interfaces;
+
+public interface IGenericRepository<TEntity>
+{
+    Task<ICollection<TEntity>> GetAllAsync();
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> criteria);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task AddAsync(TEntity entity);
+}
