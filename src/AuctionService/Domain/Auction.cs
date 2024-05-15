4ccc621c5ba41b7daebee3329a35efb2266c05e3
Wale <onafesowale@gmail.com>
2024-05-15 17:20:26 +0100

    renamed prop

--- src/AuctionService/Domain/Auction.cs
+++ src/AuctionService/Domain/Auction.cs
@@ -10,8 +10,8 @@ public class Auction : BaseEntity
     public string Winner { get; set; } = string.Empty;
     public decimal? SoldAmount { get; set; }
     public decimal? CurrentHighBid { get; set; }
-    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdateAt { get; set;} = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set;} = DateTime.UtcNow;
     public DateTime AuctionEnd { get; set; }
     public Status Status { get; set; }
     public Item Item { get; set; }
