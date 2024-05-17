2ee12938254ad94eb4a5fb0c5db85bb15a7a5c38
Wale <onafesowale@gmail.com>
2024-05-17 04:18:51 +0100

    IAuctionRepo

--- src/AuctionService/Interfaces/IAuctionRepository.cs
+++ src/AuctionService/Interfaces/IAuctionRepository.cs
@@ -5,4 +5,7 @@ namespace AuctionService.Interfaces;
 
 public interface IAuctionRepository : IGenericRepository<Auction>
 {
+    IUnitOfWork UnitOfWork { get; set; }
+
+    Task<ICollection<Auction>> GetAuctions();
 }
