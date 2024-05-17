734786ca9b9bd083b8a9e4f5fc83702665e56020
Wale <onafesowale@gmail.com>
2024-05-17 04:18:40 +0100

    get auctions query

---
+++ src/AuctionService/Features/Auction/Query/List/GetAuctionsQuery.cs
@@ -0,0 +1,23 @@
+﻿using AuctionService.Domain;
+using AuctionService.Interfaces;
+using MediatR;
+using Shared.Response;
+
+namespace AuctionService.Features.Auction.Query.List;
+
+public record GetAuctionsQuery : IRequest<Result<ICollection<Domain.Auction>>>;
+
+internal sealed class GetAuctionsQueryHandler : IRequestHandler<GetAuctionsQuery, Result<ICollection<Domain.Auction>>>
+{
+    private readonly IAuctionRepository _auctionRepository;
+
+    public GetAuctionsQueryHandler(IAuctionRepository auctionRepository)
+    {
+        _auctionRepository = auctionRepository;
+    }
+
+    public async Task<Result<ICollection<Domain.Auction>>> Handle(GetAuctionsQuery request, CancellationToken cancellationToken)
+    {
+        return new Result<ICollection<Domain.Auction>>(await _auctionRepository.GetAuctions());
+    }
+}
