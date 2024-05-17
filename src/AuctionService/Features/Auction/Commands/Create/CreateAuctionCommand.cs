5f823af168a8ad3164da7f94d170ba2f508b0a3c
Wale <onafesowale@gmail.com>
2024-05-17 04:18:32 +0100

    create auction command

--- src/AuctionService/Features/Auction/Commands/Create/CreateAuctionCommand.cs
+++ src/AuctionService/Features/Auction/Commands/Create/CreateAuctionCommand.cs
@@ -1,14 +1,44 @@
-﻿using MediatR;
+﻿using AuctionService.Dtos;
+using AuctionService.Interfaces;
+using MediatR;
 using Shared.Response;
 
 namespace AuctionService.Features.Auction.Commands.Create;
 
-internal record CreateAuctionCommand() : IRequest<Result<bool>>;
+internal record CreateAuctionCommand(CreateAuctionDto Auction) : IRequest<Result<bool>>;
 
 internal sealed class CreateAuctionCommandHandler : IRequestHandler<CreateAuctionCommand, Result<bool>>
 {
+    private readonly IAuctionRepository _auctionRepository;
+
+    public CreateAuctionCommandHandler(IAuctionRepository auctionRepository)
+    {
+        _auctionRepository = auctionRepository;
+    }
+
     public async Task<Result<bool>> Handle(CreateAuctionCommand request, CancellationToken cancellationToken)
     {
-        return await Task.FromResult(true);
+        await _auctionRepository.AddAsync(new Domain.Auction
+        {
+            ReservePrice = request.Auction.ReservePrice,
+            Seller = request.Auction.Seller,
+            AuctionEnd = request.Auction.AuctionEnd,
+            Status = request.Auction.Status,
+            Item = new()
+            {
+                Value = request.Auction.Value,
+                Medium = request.Auction.Medium,
+                Artist = request.Auction.ItemArtist,
+                CreatedAt = request.Auction.ItemCreatedAt,
+                ImageUrl = request.Auction.ImageUrl,
+                Dimension = new(request.Auction.Height, request.Auction.Width, request.Auction.Depth),
+                CurrentLocation = request.Auction.CurrentLocation,
+                IsAuthenticated = request.Auction.IsAuthenticated,
+                Style = request.Auction.Style,
+                Title = request.Auction.ItemTitle
+            }
+        });
+
+        return await _auctionRepository.UnitOfWork.CompleteAsync() > 0;
     }
 }
