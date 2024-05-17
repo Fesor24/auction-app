ce751ad75324a9e279ad8b4f5e64d6969cf3924d
Wale <onafesowale@gmail.com>
2024-05-17 04:18:17 +0100

    create auction dto

---
+++ src/AuctionService/Dtos/CreateAuctionDto.cs
@@ -0,0 +1,23 @@
+﻿using AuctionService.Enums;
+
+namespace AuctionService.Dtos;
+
+public class CreateAuctionDto
+{
+    public decimal ReservePrice { get; set; }
+    public string Seller { get; set; }
+    public DateTime AuctionEnd { get; set; }
+    public Status Status { get; set; }
+    public string ItemTitle { get; set; }
+    public string ItemArtist { get; set; }
+    public decimal Height { get; set; }
+    public decimal Width { get; set; }  
+    public decimal Depth { get; set; }
+    public Style Style { get; set; }
+    public Medium Medium { get; set; }
+    public string CurrentLocation { get; set; }
+    public decimal Value { get; set; }
+    public string ImageUrl { get; set; }
+    public bool IsAuthenticated { get; set; }
+    public DateTime ItemCreatedAt { get; set; }
+}
