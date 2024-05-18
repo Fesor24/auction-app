ef84397abae2c80661f78435cdda44898be8397f
Wale <onafesowale@gmail.com>
2024-05-18 02:46:10 +0100

    Delete auction functionality

---
+++ src/AuctionService/Features/Auction/Commands/Delete/DeleteAuctionCommand.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Interfaces;
+using MediatR;
+using Shared.Response;
+
+namespace AuctionService.Features.Auction.Commands.Delete;
+
+public record DeleteAuctionCommand(Guid AuctionId) : IRequest<Result<bool>>;
+
+internal sealed class DeleteAuctionCommandHandler : IRequestHandler<DeleteAuctionCommand, Result<bool>>
+{
+    private readonly IAuctionRepository _auctionRepository;
+
+    public DeleteAuctionCommandHandler(IAuctionRepository auctionRepository)
+    {
+        _auctionRepository = auctionRepository;
+    }
+
+    public async Task<Result<bool>> Handle(DeleteAuctionCommand request, CancellationToken cancellationToken)
+    {
+        var auction = await _auctionRepository.GetAsync(x => x.Id == request.AuctionId);
+
+        if (auction is null)
+            return Error.NotFound("AUCTION_NOTFOUND", "Auction not found");
+
+        _auctionRepository.Delete(auction);
+
+        return await _auctionRepository.UnitOfWork.CompleteAsync() > 0;
+    }
+}
