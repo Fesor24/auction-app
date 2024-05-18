9534c47ed85932f7fd6bc65ebda4eafb5d45d8f2
Wale <onafesowale@gmail.com>
2024-05-18 02:45:58 +0100

    Create auction functionality

--- src/AuctionService/Features/Auction/Commands/Create/CreateAuctionCommand.cs
+++ src/AuctionService/Features/Auction/Commands/Create/CreateAuctionCommand.cs
@@ -1,13 +1,30 @@
 ﻿using AuctionService.Dtos;
 using AuctionService.Interfaces;
+using FluentValidation;
 using MediatR;
 using Shared.Response;
 
 namespace AuctionService.Features.Auction.Commands.Create;
 
-internal record CreateAuctionCommand(CreateAuctionDto Auction) : IRequest<Result<bool>>;
+internal record CreateAuctionCommand(CreateAuctionDto Auction) : IRequest<Result<Guid>>;
 
-internal sealed class CreateAuctionCommandHandler : IRequestHandler<CreateAuctionCommand, Result<bool>>
+internal class CreateAuctionCommandValidator : AbstractValidator<CreateAuctionCommand>
+{
+    public CreateAuctionCommandValidator()
+    {
+        RuleFor(x => x.Auction.ImageUrl)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Image url can not be null or empty");
+
+        RuleFor(x => x.Auction.ItemTitle)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Title can not be null or empty");
+    }
+}
+
+internal sealed class CreateAuctionCommandHandler : IRequestHandler<CreateAuctionCommand, Result<Guid>>
 {
     private readonly IAuctionRepository _auctionRepository;
 
@@ -16,12 +33,12 @@ internal sealed class CreateAuctionCommandHandler : IRequestHandler<CreateAuctio
         _auctionRepository = auctionRepository;
     }
 
-    public async Task<Result<bool>> Handle(CreateAuctionCommand request, CancellationToken cancellationToken)
+    public async Task<Result<Guid>> Handle(CreateAuctionCommand request, CancellationToken cancellationToken)
     {
-        await _auctionRepository.AddAsync(new Domain.Auction
+        Domain.Auction auction = new()
         {
             ReservePrice = request.Auction.ReservePrice,
-            Seller = request.Auction.Seller,
+            Seller = "Test", // will be gotten from context later
             AuctionEnd = request.Auction.AuctionEnd,
             Status = request.Auction.Status,
             Item = new()
@@ -37,8 +54,12 @@ internal sealed class CreateAuctionCommandHandler : IRequestHandler<CreateAuctio
                 Style = request.Auction.Style,
                 Title = request.Auction.ItemTitle
             }
-        });
+        };
 
-        return await _auctionRepository.UnitOfWork.CompleteAsync() > 0;
+        await _auctionRepository.AddAsync(auction);
+
+        await _auctionRepository.UnitOfWork.CompleteAsync();
+
+        return auction.Id;
     }
 }
