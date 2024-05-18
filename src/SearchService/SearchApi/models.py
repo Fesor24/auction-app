c354416dfdd512e0a3aee85641b51ac4b5ce3500
Wale <onafesowale@gmail.com>
2024-05-18 19:07:15 +0100

    auction model

--- src/SearchService/SearchApi/models.py
+++ src/SearchService/SearchApi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Auction(models.Model):
+    auction_id = models.CharField(max_length=50)
+    title = models.CharField(max_length=70)
+    description = models.CharField(max_length=120)
+    seller = models.CharField(max_length=50)
+    buyer = models.CharField(max_length=50, default='')
+    status = models.CharField(max_length=10, default='')
+
+    def __str__(self):
+        return self.title
