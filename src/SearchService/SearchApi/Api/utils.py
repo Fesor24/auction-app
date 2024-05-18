cc016d6f884e1d6bde0f849ea478e6b164d7658d
Wale <onafesowale@gmail.com>
2024-05-18 19:20:32 +0100

    rename method

--- src/SearchService/SearchApi/Api/utils.py
+++ src/SearchService/SearchApi/Api/utils.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 
-def get_db_handler():
-    client = MongoClient(host='localhost', port=27017)
+port = 27017
+
+def get_search_collection():
+    client = MongoClient(host='localhost', port=port)
     db = client['search_auction']
     collection = db['search']
     indexes = collection.index_information()
