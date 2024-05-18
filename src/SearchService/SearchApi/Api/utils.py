30373a75a90e084ad3aa28c2a117d2c3fe591680
Wale <onafesowale@gmail.com>
2024-05-18 19:13:17 +0100

    set indexes

--- src/SearchService/SearchApi/Api/utils.py
+++ src/SearchService/SearchApi/Api/utils.py
@@ -4,4 +4,9 @@ def get_db_handler():
     client = MongoClient(host='localhost', port=27017)
     db = client['search_auction']
     collection = db['search']
+    indexes = collection.index_information()
+    if not 'auction_id_1' in indexes:
+        collection.create_index([('auction_id', 1)], unique=True)
+    collection.create_index([('title', 1)], unique=False)
+    
     return collection
