64ba66361328f01dc22afd1b1aae02ff1a523d52
Wale <onafesowale@gmail.com>
2024-05-18 02:46:50 +0100

    mapings

---
+++ src/AuctionService/Mappings/AuctionMapping.cs
@@ -0,0 +1,13 @@
+﻿using AuctionService.Domain;
+using AuctionService.Dtos;
+using AutoMapper;
+
+namespace AuctionService.Mappings;
+
+public class AuctionMapping : Profile
+{
+    public AuctionMapping()
+    {
+        CreateMap<Auction, GetAuctionDto>();
+    }
+}
---
+++ src/AuctionService/Mappings/ItemMapping.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.Domain;
+using AuctionService.Dtos;
+using AutoMapper;
+
+namespace AuctionService.Mappings;
+
+public class ItemMapping : Profile
+{
+    public ItemMapping()
+    {
+        CreateMap<Item, GetAuctionItemDto>()
+            .ForMember(d => d.Height, f => f.MapFrom(s => s.Dimension.Height))
+            .ForMember(d => d.Depth, f => f.MapFrom(s => s.Dimension.Depth))
+            .ForMember(d => d.Width, f => f.MapFrom(s => s.Dimension.Width));
+    }
+}
