52197aa2f688aab156fa9acfdb5d74597261b123
Wale <onafesowale@gmail.com>
2024-05-18 19:13:37 +0100

    add path

--- src/SearchService/SearchApi/Api/urls.py
+++ src/SearchService/SearchApi/Api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MovieSearch.as_view(), name="movie-search")
+    path('', views.MovieSearch.as_view(), name="movie-search"),
+    path('<id>', views.MovieSearchItem.as_view(), name="movie_search_item")
 ]
