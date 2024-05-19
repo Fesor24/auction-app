89796e5320120e87398afc5a458d0a73d924e22d
Wale <onafesowale@gmail.com>
2024-05-19 06:31:50 +0100

    change model type

--- src/SearchService/SearchApi/models.py
+++ src/SearchService/SearchApi/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Auction(models.Model):
-    auction_id = models.CharField(max_length=50)
+    auction_id = models.UUIDField(max_length=50, auto_created=False)
     title = models.CharField(max_length=70)
     description = models.CharField(max_length=120)
     seller = models.CharField(max_length=50)
     buyer = models.CharField(max_length=50, default='')
-    status = models.CharField(max_length=10, default='')
+    status = models.CharField(max_length=20, default='')
 
     def __str__(self):
         return self.title
