86ad786444e64b34073ef201f54a683a42dbc96d
Wale <onafesowale@gmail.com>
2024-05-19 06:31:02 +0100

    modified urls

--- src/SearchService/SearchApi/Api/urls.py
+++ src/SearchService/SearchApi/Api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MovieSearch.as_view(), name="movie-search"),
-    path('<id>', views.MovieSearchItem.as_view(), name="movie_search_item")
+    path('', views.AuctionSearch.as_view(), name="movie-search"),
+    path('<id>', views.AuctionSearchItem.as_view(), name="movie_search_item")
 ]
