b20ed581e47925cc3ecf26202c8167637d4ac623
Wale <onafesowale@gmail.com>
2024-05-19 13:46:04 +0100

    check for more query params

--- src/SearchService/SearchApi/Api/views.py
+++ src/SearchService/SearchApi/Api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from pymongo import ASCENDING
 from . import utils
 from . import serializers
 from .. import models
+from datetime import datetime, timedelta, timezone
 
 collection = utils.get_search_collection()
 
@@ -11,6 +13,11 @@ class AuctionSearch(APIView):
     def get(self, request):
         page_number = request.GET.get('pageNumber', 1)
         page_size = request.GET.get('pageSize', 10)
+        style = request.GET.get('style', '')
+        medium = request.GET.get('medium', '')
+        title = request.GET.get('title', '')
+        finished = request.GET.get('finished', False)
+        endingSoon = request.GET.get('endingSoon', False)
 
         try:
             page_number = int(page_number)
@@ -21,9 +28,28 @@ class AuctionSearch(APIView):
         
         filter = {}
 
+        if len(style) > 0:
+            filter['style'] = {'$regex': style, '$options': 'i'}
+
+        if len(medium) > 0:
+            filter['medium'] = {'$regex': medium, '$options': 'i'}
+
+        if len(title) > 0:
+            filter['title'] = {'$regex': title, '$options': 'i'}
+
+        current_datetime = datetime.now(timezone.utc)
+
+        one_hour_ago = current_datetime - timedelta(hours=1)
+
+        if not finished and endingSoon:
+            filter['auction_end'] = {'$gt': one_hour_ago, '$lt': current_datetime}
+
+        if finished:
+            filter['auction_end'] = {'$lt': current_datetime}
+
         skip = (page_number - 1) * page_size
 
-        auctions = collection.find(filter).skip(skip).limit(page_size)
+        auctions = collection.find(filter).skip(skip).limit(page_size).sort('title', ASCENDING)
         record_count = collection.count_documents(filter)
         serializer = serializers.AuctionSerializer(auctions, many=True)
         return Response({"data": serializer.data,
@@ -36,7 +62,7 @@ class AuctionSearch(APIView):
         serializer = serializers.AuctionSerializer(data=request.data)
 
         if serializer.is_valid():
-            collection.insert_one(serializer.data)
+            collection.insert_one(serializer.validated_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -59,8 +85,10 @@ class AuctionSearchItem(APIView):
         data = request.data
         seller = data['seller'] # update these two for now
         auction_status = data['status']
-        result = collection.update_one({'auction_id': id}, {'$set':{'seller': seller, 
-                                                                    'status': auction_status}})
+        result = collection.update_one(
+            {'auction_id': id}, 
+            {'$set':{'seller': seller, 
+                    'status': auction_status}})
 
         if result.modified_count > 0:
             return Response(status=status.HTTP_204_NO_CONTENT)
