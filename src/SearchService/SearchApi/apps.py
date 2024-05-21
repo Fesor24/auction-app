8927ec2b1b8dc34286dfe089ba404ee4bf19595f
Wale <onafesowale@gmail.com>
2024-05-21 06:45:39 +0100

    load database with data

--- src/SearchService/SearchApi/apps.py
+++ src/SearchService/SearchApi/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+from . import utils
 
 class SearchapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'SearchApi'
+
+    def ready(self):
+        import os
+
+        # on first build, this will return None, for subsequent auto reload, it will return true
+        # we only want the code to run once
+        # hence this check
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            try:
+                utils.load_db_data()
+            except Exception as e:
+                print(f'An error occurred. Details: {e}')
