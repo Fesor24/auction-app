7826efc3888ffad0eff6d17481c62f35f5d4c222
Wale <onafesowale@gmail.com>
2024-05-24 05:43:37 +0100

    auction created class

---
+++ src/EventBus.Messages/AuctionCreated.cs
@@ -0,0 +1,26 @@
+﻿namespace EventBus.Messages;
+public class AuctionCreated
+{
+    public Guid Id { get; set; }
+    public decimal ReservePrice { get; set; }
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public decimal SoldAmount { get; set; }
+    public decimal CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime AuctionEnd { get; set; }
+    public string Status { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+    public decimal Width { get; set; }
+    public decimal Height { get; set; }
+    public decimal Depth { get; set; }
+    public DateTime ItemCreatedAt { get; set; }
+    public string Style { get; set; }
+    public string Medium { get; set; }
+    public string CurrentLocation { get; set; }
+    public decimal Value { get; set; }
+    public string ImageUrl { get; set; }
+    public bool IsAuthenticated { get; set; }
+}
